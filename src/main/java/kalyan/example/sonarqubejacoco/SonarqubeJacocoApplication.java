3ea3d431456b43c520ecc132ae9e0a233c569316
“kalyan” <ka20227334@wipro.com>
2021-08-25 07:32:31 +0530

    Initial commit

---
+++ src/main/java/kalyan/example/sonarqubejacoco/SonarqubeJacocoApplication.java
@@ -0,0 +1,13 @@
+package kalyan.example.sonarqubejacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeJacocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeJacocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kalyan/example/sonarqubejacoco/SonarqubeJacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package kalyan.example.sonarqubejacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeJacocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
